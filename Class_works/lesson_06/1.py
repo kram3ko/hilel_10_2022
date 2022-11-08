69e339787853905b970e46e06086d0785533948c
Vinohradov Volodymyr <eXpertt13@gmail.com>
2022-11-08 12:20:13 +0200

    add classworks

---
+++ Class_works/lesson_06/1.py
@@ -0,0 +1,15 @@
+from typing import Callable
+
+
+def greeting():
+    print("Hello")
+
+
+another_greeting = greeting
+
+
+def another(something: Callable) -> None:
+    something()
+
+
+another(greeting)
